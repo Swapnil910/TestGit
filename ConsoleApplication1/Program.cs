085164967347f6a8ae7bdd846b6797fecd9e81ce
vijay jaiswal <aspdotnetvijay04@gmail.com>
2018-01-14 12:54:20 +0530

    Entere new

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Vijay do some changes !!");
+            Console.WriteLine("hello");
         }
     }
 }
